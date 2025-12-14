2dea0d059e91648a18fd0f4b068af04ed51013f8
Stannnnn <stann@live.nl>
2025-12-14 02:58:36 +0100

    Fix

--- src/hooks/Jellyfin/useJellyfinRecentlyPlayed.ts
+++ src/hooks/Jellyfin/useJellyfinRecentlyPlayed.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 import { MediaItem } from '../../api/jellyfin'
 import { useJellyfinContext } from '../../context/JellyfinContext/JellyfinContext'
 
-export const useJellyfinRecentlyPlayed = (startIndex = 0, limit = 42) => {
+export const useJellyfinRecentlyPlayed = (startIndex = 0, limit = 12) => {
     const api = useJellyfinContext()
 
     const { data, isLoading, error } = useQuery<MediaItem[], Error>({
