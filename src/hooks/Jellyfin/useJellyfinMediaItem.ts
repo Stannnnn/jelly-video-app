0953455d0e3040143202c8864857ca07e8ef25a3
Stannnnn <stann@live.nl>
2026-09-21 22:23:33 +0200

    Fix offline view

--- src/hooks/Jellyfin/useJellyfinMediaItem.ts
+++ src/hooks/Jellyfin/useJellyfinMediaItem.ts
@@ -1,16 +1,28 @@
 import { useQuery } from '@tanstack/react-query'
+import { useAudioStorageContext } from '../../context/AudioStorageContext/AudioStorageContext'
 import { useJellyfinContext } from '../../context/JellyfinContext/JellyfinContext'
 
 export const useJellyfinMediaItem = (itemId: string | undefined) => {
     const api = useJellyfinContext()
+    const audioStorage = useAudioStorageContext()
 
     const { data, isFetching, isPending, error } = useQuery({
         queryKey: ['mediaItem', itemId],
         queryFn: async () => {
             if (!itemId) throw new Error('Item ID is required')
+            if (!navigator.onLine) {
+                const track = await audioStorage.getTrack(itemId)
+                if (!track) throw new Error('No offline data available')
+                const item = { ...track.mediaItem }
+                if (track.type === 'video' && track.mediaSources) {
+                    item.MediaSources = track.mediaSources
+                }
+                return item
+            }
             return await api.getItemById(itemId)
         },
         enabled: !!itemId,
+        networkMode: 'always',
     })
 
     return {
